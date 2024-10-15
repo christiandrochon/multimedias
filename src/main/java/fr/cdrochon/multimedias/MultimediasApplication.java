29f867961826772b27f21c53b22294eaf1fe5f54
cdn <labium@tuta.io>
2024-10-15 20:45:41 +0200

    commit init

---
+++ src/main/java/fr/cdrochon/multimedias/MultimediasApplication.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.multimedias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediasApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MultimediasApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/fr/cdrochon/multimedias/MultimediasApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.multimedias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediasApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
