a1593afb3f9ce7415c217d764792051f5de7c9ac
christiandrochon <christian.drochon.auditeur@lecnam.net>
2024-10-18 16:05:49 +0200

    Preparation du 1er exercice sur les couleurs et histogramme

--- src/main/java/fr/cdrochon/multimedias/images/MultimediaController.java
+++ src/main/java/fr/cdrochon/multimedias/images/MultimediaController.java
@@ -1,4 +1,4 @@
-package fr.cdrochon.multimedias.controller;
+package fr.cdrochon.multimedias.images;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
