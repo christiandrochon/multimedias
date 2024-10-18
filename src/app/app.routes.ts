91c80877f2038c7f88a76e85f435322520b3d566
christiandrochon <christian.drochon.auditeur@lecnam.net>
2024-10-18 02:00:53 +0200

    comme javafx est tres lourd et tres difficilement configurable, j'ai changé. Swing pouvait aller mais finalement, j'ai préféré opter pour un site web car il sera potentiellement integrable dans une archi microservices.
    Le client est codé avec angular. Il est standalone comme ca se fait desormais (pas de module).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MultimediaComponent} from './multimedia/multimedia.component';  // Chemin correct pour MultimediaComponent
+import {AppComponent} from './app.component';
+import {HelloComponent} from './hello/hello.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'images', component: MultimediaComponent},
+  {path: 'hello', component: HelloComponent}
+];
