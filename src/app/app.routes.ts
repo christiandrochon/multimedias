00d3dd61d723df4cc657d1941a9a2dfd1cffc668
christiandrochon <christian.drochon.auditeur@lecnam.net>
2024-10-19 20:10:52 +0200

    Creation d'un dmaier de couleur rougeet verte
    Intervertir les couleurs RGB vers GRB d'une miage aleatoire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import {Routes} from '@angular/router';
 import {MultimediaComponent} from './multimedia/multimedia.component';  // Chemin correct pour MultimediaComponent
 import {AppComponent} from './app.component';
 import {HelloComponent} from './hello/hello.component';
+import {CouleursComponent} from './couleurs/couleurs.component';
 
 export const appRoutes: Routes = [
   {path: '', component: AppComponent},
   {path: 'images', component: MultimediaComponent},
-  {path: 'hello', component: HelloComponent}
+  {path: 'hello', component: HelloComponent},
+  {path: 'createCheckerboard', component: CouleursComponent},
+  {path: 'swapRandomImage', component: CouleursComponent}
 ];
